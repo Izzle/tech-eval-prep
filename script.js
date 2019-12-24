a2571eab5281f22940fc1a39bef12ae72b62461d
Ron Martin <ronmartin89@gmail.com>
2019-12-24 05:25:28 -0800

    Added many javascript challenges

---
+++ script.js
@@ -0,0 +1,86 @@
+'use strict';
+
+function stringReturns(words) {
+    return words;
+}
+
+console.log(stringReturns("hello"));
+
+// create a function that takes a number argument
+// and returns that value plus 5.
+// print the result to the console.
+
+function addFive(num) {
+    return num + 5;
+}
+
+console.log(addFive(3));
+
+// loop through an array and print the index and the value
+// to the console
+function arrayLooper() {
+    const myArray = ["apple", "orange", "cat"];
+
+    for(let i=0; i < myArray.length; i++) {
+        console.log(`Index: ${i} Value: ${myArray[i]}`);
+    }
+}
+
+arrayLooper();
+
+// Create a javascript object and access a value in the object 
+
+const myObj = {
+    name: 'The Object',
+    age: 22,
+    hair: 'Perfect'
+}
+
+console.log(myObj.hair);
+
+// Loop through a javascript object and print the key and value to the console
+
+function objectLooper(anything) {
+    const objectSize = Object.keys(anything);
+    for(let i=0; i < objectSize; i++) {
+        console.log(`Key: ${objectSize} Value: ${objectSize[i]}`)
+    }
+}
+
+objectLooper(myObj);
+
+// OR
+
+Object.keys(myObj).forEach(key => console.log(`${key}: ${myObj[key]}`));
+
+// Write a function that accepts a "searchName" argument. Loop through an array of Objects
+// and return the object that matches against the "name" key
+
+const myData = [
+    {
+      name: "Tom",
+      age: 22,
+      favoriteFood: "pizza"
+    },
+    {
+      name: "Jane",
+      age: 42,
+      favoriteFood: "sushi"
+    },
+    {
+      name: "Fred",
+      age: 34,
+      favoriteFood: "lettuce"
+    }
+  ];
+
+  function findByName(searchName) {
+      for(let i = 0; i < myData.length; i++) {
+        if(searchName === myData[i].name) {
+            return myData[i];
+        }
+      }
+  }
+
+  const person = findByName("Fred");
+  console.log(person);
